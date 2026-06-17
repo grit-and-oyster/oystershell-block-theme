a8bdfa7d5ec22bafbdfe8b10b658c7a4cd4ad0b6
Andy <code@grit-oyster.co.uk>
2026-06-17 10:26:59 +0100

    Updated search template - added hidden-search.php pattern

---
+++ patterns/hidden-search.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Title: Search
+ * Slug: oystershell-block-theme/hidden-search
+ * Inserter: no
+ */
+?>
+
+<!-- wp:search {"label":"<?php echo esc_attr_x('Search', 'search form label', 'oystershell-block-theme'); ?>","showLabel":false,"buttonText":"<?php echo esc_attr_x('Search', 'search button text', 'oystershell-block-theme'); ?>","fontSize":"medium"} /-->
