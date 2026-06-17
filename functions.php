b7999eb8413c0a0093f0a0eb71634195aeb7e16c
Andy <code@grit-oyster.co.uk>
2026-06-17 09:15:10 +0100

    Added oystershell_block_theme_remove_core_patterns function

--- functions.php
+++ functions.php
@@ -9,3 +9,19 @@
  * @subpackage OysterShell_Block_Theme
  * @since OysterShell Block Theme 1.0
  */
+
+if (! function_exists('oystershell_block_theme_remove_core_patterns')) :
+    /**
+     * Removes core block patterns.
+     *
+     * @since OysterShell Block Theme 1.0
+     * @return void
+     */
+    add_action('after_setup_theme', 'oystershell_block_theme_remove_core_patterns');
+
+    function oystershell_block_theme_remove_core_patterns()
+    {
+        remove_theme_support('core-block-patterns');
+    }
+
+endif;
