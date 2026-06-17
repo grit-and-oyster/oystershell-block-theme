6eb1eda2c6b2e7af95250038b9d0d8a11450e7d6
Andy <code@grit-oyster.co.uk>
2026-06-17 09:14:12 +0100

    Added functions.php file

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * OysterShell Block Theme functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package WordPress
+ * @subpackage OysterShell_Block_Theme
+ * @since OysterShell Block Theme 1.0
+ */
