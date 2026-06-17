55bcfa0deda2f3448b4e14a9ee0775260a58d789
Andy <code@grit-oyster.co.uk>
2026-06-17 11:06:09 +0100

    Added the oystershell_block_theme_register_pattern_categories function

--- functions.php
+++ functions.php
@@ -25,3 +25,22 @@ if (! function_exists('oystershell_block_theme_remove_core_patterns')) :
     }
 
 endif;
+
+if (! function_exists('oystershell_block_theme_register_pattern_categories')) :
+    /**
+     * Registers custom block pattern categories.
+     *
+     * @since OysterShell Block Theme 1.0
+     * @return void
+     */
+    add_action('init', 'oystershell_block_theme_register_pattern_categories');
+
+    function oystershell_block_theme_register_pattern_categories()
+    {
+        register_block_pattern_category('oystershell-block-theme/core', array(
+            'label'       => __('OysterShell Block Theme: Core', 'oystershell-block-theme'),
+            'description' => __('Core patterns for OysterShell Block Theme.', 'oystershell-block-theme')
+        ));
+    }
+
+endif;
