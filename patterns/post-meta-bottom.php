55a749c3e9575cff0ecb2017717500626f82f2ea
Andy <code@grit-oyster.co.uk>
2026-06-18 11:47:56 +0100

    Imporved the output of term post meta

--- patterns/post-meta-bottom.php
+++ patterns/post-meta-bottom.php
@@ -14,10 +14,7 @@
 
 <!-- wp:group {"style":{"spacing":{"blockGap":"0.2em","margin":{"bottom":"var:preset|spacing|60"}}},"layout":{"type":"flex","flexWrap":"wrap"}} -->
 <div class="wp-block-group">
-    <!-- wp:paragraph -->
-    <p><?php esc_html_e('Posted in ', 'oystershell-block-theme'); ?></p>
-    <!-- /wp:paragraph -->
-    <!-- wp:post-terms {"term":"category"} /-->
+    <!-- wp:post-terms {"term":"category","prefix":"<?php echo esc_html_x('Posted in ', 'Prefix for the post category block: Posted in', 'oystershell-block-theme'); ?>"} /-->
     <!-- wp:post-terms {"term":"post_tag","prefix":"<?php echo esc_html_x('and tagged ', 'Prefix for the post tag block: and tagged', 'oystershell-block-theme'); ?>"} /-->
 </div>
 <!-- /wp:group -->
